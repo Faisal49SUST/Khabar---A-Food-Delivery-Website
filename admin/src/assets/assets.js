920e9630e157083ebf8188c8b2f6ef4b7574ec25
Faisal49SUST <faisalahmed3205@gmail.com>
2025-11-30 16:28:37 +0600

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://khabar-a-food-delivery-website-backend.onrender.com'
+export const url = 'https://khabar-a-food-delivery-website-backend-ys94.onrender.com'
